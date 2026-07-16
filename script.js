af90e81dedc4b13182649bcd26589c537a3f79ae
faizanrex710-max <faizanrex710@gmail.com>
2026-07-16 14:02:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  console.log("Daily Viral Videos Loaded");
-
   const tg = window.Telegram?.WebApp;
 
   if (tg) {
@@ -9,12 +7,23 @@ document.addEventListener("DOMContentLoaded", () => {
     tg.expand();
   }
 
-  const button = document.querySelector("button");
+  // Search
+  const search = document.querySelector(".search");
+  const cards = document.querySelectorAll(".card");
 
-  if (button) {
-    button.addEventListener("click", () => {
-      window.open("https://t.me/VIRALVIDEOS_8", "_blank");
+  if (search) {
+    search.addEventListener("keyup", () => {
+      const value = search.value.toLowerCase();
+
+      cards.forEach(card => {
+        const text = card.innerText.toLowerCase();
+        card.style.display = text.includes(value) ? "block" : "none";
+      });
     });
   }
 
 });
+
+function openChannel() {
+  window.open("https://t.me/VIRALVIDEOS_8", "_blank");
+}
