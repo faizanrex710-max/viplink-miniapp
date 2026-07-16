0b3770ff6ce36a2989e355501606e98fab1e6a8b
faizanrex710-max <faizanrex710@gmail.com>
2026-07-16 14:50:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
+import { db } from "./firebase.js";
+
+import {
+  collection,
+  getDocs,
+  query,
+  orderBy
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
 document.addEventListener("DOMContentLoaded", async () => {
 
   const container = document.querySelector(".container");
 
   try {
 
-    const res = await fetch("./posts.json");
-    const posts = await res.json();
+    const q = query(collection(db, "posts"), orderBy("createdAt", "desc"));
+    const snapshot = await getDocs(q);
 
     let html = `
       <input type="text" class="search" placeholder="🔍 Search Videos...">
+
       <div class="category">
         <button>🔥 Trending</button>
         <button>😂 Funny</button>
@@ -17,23 +27,25 @@ document.addEventListener("DOMContentLoaded", async () => {
       </div>
     `;
 
-    posts.forEach(post => {
+    snapshot.forEach((doc) => {
+
+      const post = doc.data();
 
       html += `
       <div class="card">
+
         <img src="${post.image}" alt="${post.title}">
 
         <div class="content">
-          <div class="title">${post.title}</div>
 
-          <div class="desc">
-            👁️ ${post.views} • ⏰ ${post.time}
-          </div>
+          <div class="title">${post.title}</div>
 
           <button onclick="window.open('${post.link}','_blank')">
             📥 Download Now
           </button>
+
         </div>
+
       </div>
       `;
 
@@ -42,8 +54,8 @@ document.addEventListener("DOMContentLoaded", async () => {
     container.innerHTML = html;
 
   } catch (e) {
-    container.innerHTML = "<h3>Posts loading failed.</h3>";
     console.error(e);
+    container.innerHTML = "<h2>Error Loading Posts</h2>";
   }
 
 });
