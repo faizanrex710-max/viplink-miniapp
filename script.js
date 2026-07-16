640a52516ba0baa780978f9b5665dda5c7da248d
faizanrex710-max <faizanrex710@gmail.com>
2026-07-16 13:58:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("VIP Link Mini App Loaded");
+document.addEventListener("DOMContentLoaded", () => {
+
+  console.log("Daily Viral Videos Loaded");
+
+  const tg = window.Telegram?.WebApp;
+
+  if (tg) {
+    tg.ready();
+    tg.expand();
+  }
+
+  const button = document.querySelector("button");
+
+  if (button) {
+    button.addEventListener("click", () => {
+      window.open("https://t.me/VIRALVIDEOS_8", "_blank");
+    });
+  }
+
 });
